c43156b8ae3a7f46a3c459b9e1303e25cd165cec
amoa400-inno <amoa400-inno@amoa400-inno.local>
2014-07-17 10:34:50 +0800

    improve server & connection

--- test/index.js
+++ test/index.js
@@ -1,11 +1,35 @@
+var util = require('util');
 var windlike = require('../index');
 
 var server = windlike.createServer();
 
-server.on('subscribe', function(conn, topic, qos, messageId) {
-	conn.subscribe(topic, qos, messageId);
+var log = function(conn, action) {
+	util.log((conn.id + ' ' + conn.token + ' ' + action).blue);
+}
+
+server.on('connection', function(conn) {
+	log(conn, 'connected');
 });
 
+server.on('subscribe', function(conn, topic) {
+	log(conn, 'subscribe');
+	conn.subscribe(topic);
+});
+
+server.on('unsubscribe', function(conn, topic) {
+	log(conn, 'unsubscribe');
+});
+
+
 server.on('publish', function(conn, topic, message) {
+	log(conn, 'publish');
 	conn.publish(topic, message);
+});
+
+server.on('close', function(conn) {
+	log(conn, 'close');
+});
+
+server.on('error', function(conn) {
+	log(conn, 'error');
 });
