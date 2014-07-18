2abcb3c2cd65affd69c8cf9186e583d03235b596
amoa400 <amoa400@163.com>
2014-07-18 10:28:10 +0800

    add unit test

---
+++ test/server.js
@@ -0,0 +1,29 @@
+// requires
+var should = require('should');
+var mqtt = require('mqtt');
+var Server = require('../lib/server');
+
+describe('Server', function() {
+	it('should clean dead connections', function(done) {
+		var server = new Server();
+
+		var client = [];
+		for (var i = 0; i < 10; i++) {
+			client[i] = mqtt.connect('mqtt://localhost:1883?clientId=mqtt_client');
+		}
+
+		setTimeout(function() {
+			var now = new Date();
+			server.deadTime = 5;
+			var tot = 10;
+			for (var i in server.conns) {
+				server.conns[i].time = new Date(now - tot * 1000);
+				tot--;
+			}
+			server._clean();
+			server.connsCount.should.approximately(5, 1);
+			done();
+		}, 500);
+	});
+});
+
