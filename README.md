282b87a21371c2280b0974cec5f3c9f3d12a9916
amoa400 <amoa400@163.com>
2014-07-23 16:58:27 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-WindLike  
-a realtime communication library for mobile & browser  
+### WindLike
+***
+WindLike is a realtime communication library for browser(web app) and mobile(native app).
+
+As for browser, WindLike uses long-polling and websocket as communication method.  
+As for mobile, WindLike uses MQTT protocol as communication method. 
+
+***
+
+### How to install?  
+    npm install windlike
+
+### API
+
+[Server API](https://github.com/amoa400/WindLike/wiki/API---Server)  
+[Browser API](https://github.com/amoa400/WindLike/wiki/API---Browser-Client)  
+Mobile API (TODO)
--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 module.exports = require('./lib/windlike');
-
-/*
-var fs = require('fs');
-
-process.on('uncaughtException', function (err) {
-  console.log("Error: ");
-  console.error(err.stack);
-
-  var error = '';
-  error += '\n';
-  error += new Date();
-  error += '\n---------------------------------------------------------------------------------------------------\n';
-  error += err.stack.toString();
-  error += '\n===================================================================================================\n';
-
-  fs.appendFile(__dirname + '/error.log', error);
-});
-*/
